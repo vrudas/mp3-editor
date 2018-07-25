4c3feb3c806e9edccc62b0e2925fcaa0013c3029
vrudas <vrudas4work@gmail.com>
2018-07-25 21:05:42 +0300

    L4H Episodes and SPD 12 Years

--- src/main/kotlin/io/playlisteditor/TrackListEditor.kt
+++ src/main/kotlin/io/playlisteditor/TrackListEditor.kt
@@ -7,13 +7,18 @@ fun main(args: Array<String>) {
 
 class TrackListEditor {
 
-    fun parseTrackList(trackList: String): String =
-        trackList.split("\n")
+    fun parseTrackListAsString(trackList: String) =
+        parseTrackList(trackList).joinToString(separator = "\n") { it.toTrackListString() }
+
+    fun parseTrackList(trackList: String): List<Track> {
+        return trackList.split("\n")
             .drop(1)
             .map { it.split("\t") }
             .map { Track(it[0].toByte(), it[1], it[2]) }
             .sortedBy { it.number }
-            .joinToString(separator = "\n") { it.toTrackListString() }
+    }
+
+    fun extractArtists(tracks: List<Track>): List<String> = tracks.map { it.artist }.distinct()
 }
 
 data class Track(val number: Byte, val title: String, val artist: String) {
@@ -136,5 +141,84 @@ object TrackLists {
         16	Mystical Expedition (Extended Mix)	Sound Rush	150.00	12A	Mystical Expedition	Techno	03:31	     	15
         17	Whistle	Stuback	150.00	11B	Whistle / Soundwave WEB	Techno	06:05	     	16
         18	Trapped in this World	Kasparov	82.28	01A	Keep Going WEB	Hardcore	06:06	     	17
+    """.trimIndent()
+
+    val EP05 = """
+        #	Track Title	Artist	BPM	Key	Album	Genre	Time	Rating	Comments
+        1	The Power of Music	Headhunterz	150.00	01A	The Leaked EP WEB	L4H	05:44	     	01
+        2	Make The World Ours	Hardwell	150.02	04A	Victory Forever EP WEB	L4H	04:08	     	02
+        3	Razor Sharp (Extended Mix)	Stephanie	150.00	04A	Razor Sharp	Hardstyle	04:25	     	03
+        4	Save Our Dream	D-Block & S-Te-Fan, The Pitcher & DV Rocks!	150.00	06A	Q-Dance Hardstyle Top 40 2013-05	L4H	04:51	     	04
+        5	Love For The Game	Coone	150.00	08A	Q-Dance Hardstyle Top 100 2015	L4H	04:12	     	05
+        6	Lockdown	Scope DJ	143.00	08A	Lockdown / Protocol	Classic Hardstyle	06:54	     	06
+        7	Freedom	Da Tweekaz & Neilio	150.00	08A	Q-Dance Hardstyle Top 40 2016-02	L4H	06:34	     	07
+        8	(We Are) Indestructible (Original Mix)	Frontliner Ft. Katt Niall	150.00	08A	(We Are) Indestructible WEB	L4H	05:38	     	08
+        9	Timeless	Wildstylez	150.00	08A	Q-Dance Hardstyle Top 40 2013-04	L4H	05:08	     	09
+        10	Luminosity	Project One	150.00	08A	EP I	L4H	04:29	*****	10
+        11	Unbreakable (Extended Mix)	Psyko Punkz & DJ Isaac and Sound Rush	149.99	09A	Unbreakable WEB	L4H	03:42	     	11
+        12	Imagine (Original Mix)	Bass Modulators	150.00	08A	Imagine WEB	L4H	04:29	     	12
+        13	Imaginary (Original Mix)	Brennan Heart & Jonathan Mendelsohn	149.99	09A	Imaginary WEB	L4H	04:58	     	13
+        14	Through The Night (Original Mix)	KELTEK	150.11	09A	Through The Night WEB	L4H	03:26	     	14
+        15	Above Average (Original Mix)	D-Block & S-Te-Fan	150.00	09A	Above Average WEB	Hardstyle	04:48	     	15
+        16	Before You Go (Original Mix)	MYST	154.01	09A	Prophecy WEB	L4R	04:40	     	16
+        17	Zombie (Original Mix)	Ran-D	155.00	09A	Zombie WEB	L4R	05:14	     	17
+        18	Encore	Wildstylez	148.00	11A	Q-Dance Hardstyle Top 40 2016-07	L4H	05:17	     	18
+        19	#MyWay	Ran-D	150.00	01A	#MyWay WEB	L4R	06:30	     	19
+        20	No Time To Waste (Defqon.1 Anthem 2010)	Wildstylez	148.00	11A	Fear FM Hardstyle Top 100 2010	Hardstyle Anthem	08:34	     	20
+        21	Lost In Dreams (Masters Of Ceremony Remix)	Mike NRG	85.00	05A	Lost In Dreams (Q-Base Anthem: The 2010 Edition)	Hardcore Anthem	05:17	     	21
+    """.trimIndent()
+
+    val EP06 = """
+        #	Track Title	Artist	BPM	Key	Time	Genre	Comments	Rating
+        1	Maximum Force (defqon.1 Australia 2009 Anthem)	Zany	150.00	06A	05:01	Hardstyle Anthem	001
+        2	Scrap Attack (DefQon.1 2009 Anthem) 	Headhunterz	150.11	10A	07:17	Hardstyle Anthem	002
+        3	No Time To Waste (Defqon.1 Anthem 2010)	Wildstylez	148.00	11A	08:34	Hardstyle Anthem	003
+        4	Unite (Official Defqon.1 Anthem 2011)	Noisecontrollers	150.00	04A	06:50	Hardstyle Anthem	004
+        5	World Of Madness (Defqon.1 Anthem 2012)	Headhunterz & Wildstylez Vs. Noisecontrollers	150.00	03A	06:25	Hardstyle Anthem	005
+        6	Scrap The System (Defqon 1 Australia 2013 Anthem)	Brennan Heart	150.00	04A	04:03	Hardstyle Anthem	006	*****
+        7	Unleash The Beast (Official Defqon 1 Australia 2014 Anthem)	Code Black	150.00	04A	05:43	Hardstyle Anthem	007
+        8	Unleash the Beast (Defqon 1 Chile Anthem 2015)	Wildstylez	148.00	02A	05:50	Hardstyle Anthem	008
+        9	Dragonblood (Defqon.1 Anthem 2016)	Bass Modulators	150.00	03A	06:54	Hardstyle Anthem	009
+        10	Dragonblood (Defqon.1 Australia Anthem 2016)	Audiofreq & Code Black & Toneshifterz	150.00	04A	06:12	Hardstyle Anthem	010
+        11	Dragonblood (Defqon.1 Chile Anthem 2016)	Frontliner	149.91	04A	05:47	Hardstyle Anthem	011
+        12	No Guts No Glory (Defqon.1 Australia Anthem 2015)	Frontliner & Dillytek feat. 360	150.00	04A	06:40	Hardstyle Anthem	012
+        13	No Guts, No Glory (Defqon.1 Anthem 2015)	Ran-D feat. Skits Vicious	150.11	04A	08:04	Hardstyle Anthem	013
+        14	True Rebel Freedom (Defqon 1 Australia 2012 Anthem)	Wildstylez	150.00	06A	06:44	Hardstyle Anthem	014
+        15	Eye Of The Storm (Defqon.1 Australia Anthem 2017) (Pro Mix)	D-Block & S-Te-Fan	149.99	08A	05:08	Hardstyle Anthem	015
+        16	Save Your Scrap for Victory (Defqon 1 Australia Anthem 2010)	Headhunterz	150.00	10B	06:15	Hardstyle Anthem	016
+        17	Survival Of The Fittest (Defqon 1 Anthem 2014)	Coone	150.00	11A	06:15	Hardstyle Anthem	017
+        18	Psychedelic Wasteland (Official Defqon.1 Australia Anthem 2011)	Toneshifterz	150.00	11A	05:29	Hardstyle Anthem	018
+        19	Weekend Warriors (Official Defqon 1 2013 Anthem)	Frontliner	150.01	11A	05:33	Hardstyle Anthem	019	*****
+        20	Victory Forever (Defqon.1 Anthem 2017)	Frequencerz	150.00	11A	05:12	Hardstyle Anthem	020
+        21	Maximum Force (Defqon.1 Anthem 2018) (Pro Mix)	Project One	150.00	08A	05:50	Hardstyle Anthem	021
+    """.trimIndent()
+
+    val SPD_12_YEARS = """
+        #	Track Title	Artist	BPM	Key	Time	Genre	Comments	Rating
+        1	Live The Night (Original Mix)	W&W, Hardwell ft. Lil Jon	128.00	04A	04:33	Big Room	001 - 108
+        2	ACDC - Thunderstruck (Shameless Bootleg)		130.00	10A	02:57	Melbourne Bounce	002 - 104
+        3	Party Till We Die (feat. Andrew W.K.) [Extended Mix]	MAKJ & Timmy Trumpet	128.00	04A	03:53	Melbourne Bounce	003 - 75
+        4	Bon Jovi - Its My Life (TuneSquad Bootleg) full		128.00	05A	03:48	Melbourne Bounce	004 - 195
+        5	DOPEDROP - We Are The Stars (Original Mix)		128.00	04A	04:12	Melbourne Bounce	005 - 90
+        6	The Hum (Original Mix)	Dimitri Vegas & Like Mike vs. Ummet Ozcan	130.00	09A	05:56	Electro House	006 - 120
+        7	Bigfoot	W&W	128.00	04A	04:30	EDM	007 - 75
+        8	Karate (Radio Edit)	R3hab & KSHMR	128.00	04A	02:43	EDM	008 - 142
+        9	Memories (feat. Sirah)	KSHMR & Bassjackers	128.00	01A	05:05	EDM	009 - 165
+        10	Freaks we will rock you (K3Leng Mashup)		128.38	09A	05:36	Big Room	010 - 222
+        11	Bassjackers, D'Angello & Francis - All Aboard (Dimitri Vegas, Like Mike Edit)	Bassjackers, L3N	130.00	09A	04:17	Big Room	011 - 91
+        12	Cutting Crew - Died in Your Arms (TuneSquad Bootleg) Full		128.00	10A	04:45	Melbourne Bounce	012 - 108
+        13	Rave-Olution (Original Mix)	Kenneth G + AudioTwinz	128.00	11A	04:59	EDM	013 - 120
+        14	Cosmic Dark vs. In The End (Hardwell UMF 2016 Mashup)	Pitchback vs. Alesso vs. David Guetta vs. Linkin Park	130.00	09A	04:22	EDM	014 - 152
+        15	Final Countdown 2k16	KEVU & Luke Alive	128.00	11A	04:07	EDM	015 - 82
+        16	Last Resort vs Smooth Criminal (Dimitri Vegas & Like Mike Mashup)	Papa Roach vs Ummet Ozcan & Michael Jackson	128.01	09A	03:49	Big Room	016 - 105
+        17	Sweat Dreams (Ummet Ozcan Remix)	Eurythmics	128.00	12A	03:20	Big Room	017 - 112
+        18	The Next Episode (Ummet Ozcan Remix)	Snoop Dogg, Ummet Ozcan	127.73	02A	03:56	Big Room	018 - 161
+        19	Pop Mix	Hardscar	128.00		10:32		9019 - 632
+        20	Omen	The Prodigy	140.00	10A	03:36	Electronic	020 - 113
+        21	Voodoo People (Pendulum Radio Edit)	The Prodigy	87.00	01A	03:16	Drum & Bass	021 - 99
+        22	Blood Sugar	Pendulum	88.00	06A	05:15	Drum & Bass	022 - 279
+        23	Bring Me To Life (Pro Mix)	Da Tweekaz ft. HALIENE	150.00	09A	05:38	L4H	023 - 229
+        24	Komon (Extended Mix)	Da Tweekaz	150.11	04A	04:37	L4H	024 - 124
+        25	Zombie (Original Mix)	Ran-D	155.00	09A	05:14	L4R	025 - 282
     """.trimIndent()
 }
--- src/test/kotlin/io/playlisteditor/TrackListEditorTest.kt
+++ src/test/kotlin/io/playlisteditor/TrackListEditorTest.kt
@@ -97,7 +97,7 @@ class TrackListEditorTest {
                 00:00 01 Adrenalize - Secrets of Time
                 00:00 02 Frontliner - Keep It Up
                 00:00 03 Kodex feat. Steklo - I'm Alive
-                00:00 04 Dillytek - Through The Light
+                00:00 04 Dillytek - through the light (extended mix)
                 00:00 05 Wildstylez - I See Stars
                 00:00 06 Noisecontrollers - So High
                 00:00 07 D-Block & S-Te-Fan - Rebel
@@ -113,11 +113,99 @@ class TrackListEditorTest {
                 00:00 17 Stuback - Whistle
                 00:00 18 Kasparov - Trapped in this World
             """.trimIndent()
+        ),
+        arrayOf(
+            TrackLists.EP05,
+            """
+                00:00 01 Headhunterz - The Power of Music
+                00:00 02 Hardwell - Make The World Ours
+                00:00 03 Stephanie - Razor Sharp
+                00:00 04 D-Block & S-Te-Fan, The Pitcher & DV Rocks! - Save Our Dream
+                00:00 05 Coone - Love For The Game
+                00:00 06 Scope DJ - Lockdown
+                00:00 07 Da Tweekaz & Neilio - Freedom
+                00:00 08 Frontliner Ft. Katt Niall - (We Are) Indestructible
+                00:00 09 Wildstylez - Timeless
+                00:00 10 Project One - Luminosity
+                00:00 11 Psyko Punkz & DJ Isaac and Sound Rush - Unbreakable
+                00:00 12 Bass Modulators - Imagine
+                00:00 13 Brennan Heart & Jonathan Mendelsohn - Imaginary
+                00:00 14 KELTEK - Through The Night
+                00:00 15 D-Block & S-Te-Fan - Above Average
+                00:00 16 MYST - Before You Go
+                00:00 17 Ran-D - Zombie
+                00:00 18 Wildstylez - Encore
+                00:00 19 Ran-D - #MyWay
+                00:00 20 Wildstylez - No Time To Waste (Defqon.1 Anthem 2010)
+                00:00 21 Mike NRG - Lost In Dreams (Masters Of Ceremony Remix)
+            """.trimIndent()
+        ),
+        arrayOf(
+            TrackLists.EP06,
+            """
+                00:00 01 Zany - Maximum Force (defqon.1 Australia 2009 Anthem)
+                00:00 02 Headhunterz - Scrap Attack (DefQon.1 2009 Anthem)
+                00:00 03 Wildstylez - No Time To Waste (Defqon.1 Anthem 2010)
+                00:00 04 Noisecontrollers - Unite (Official Defqon.1 Anthem 2011)
+                00:00 05 Headhunterz & Wildstylez Vs. Noisecontrollers - World Of Madness (Defqon.1 Anthem 2012)
+                00:00 06 Brennan Heart - Scrap The System (Defqon 1 Australia 2013 Anthem)
+                00:00 07 Code Black - Unleash The Beast (Official Defqon 1 Australia 2014 Anthem)
+                00:00 08 Wildstylez - Unleash the Beast (Defqon 1 Chile Anthem 2015)
+                00:00 09 Bass Modulators - Dragonblood (Defqon.1 Anthem 2016)
+                00:00 10 Audiofreq & Code Black & Toneshifterz - Dragonblood (Defqon.1 Australia Anthem 2016)
+                00:00 11 Frontliner - Dragonblood (Defqon.1 Chile Anthem 2016)
+                00:00 12 Frontliner & Dillytek feat. 360 - No Guts No Glory (Defqon.1 Australia Anthem 2015)
+                00:00 13 Ran-D feat. Skits Vicious - No Guts, No Glory (Defqon.1 Anthem 2015)
+                00:00 14 Wildstylez - True Rebel Freedom (Defqon 1 Australia 2012 Anthem)
+                00:00 15 D-Block & S-Te-Fan - Eye Of The Storm (Defqon.1 Australia Anthem 2017)
+                00:00 16 Headhunterz - Save Your Scrap for Victory (Defqon 1 Australia Anthem 2010)
+                00:00 17 Coone - Survival Of The Fittest (Defqon 1 Anthem 2014)
+                00:00 18 Toneshifterz - Psychedelic Wasteland (Official Defqon.1 Australia Anthem 2011)
+                00:00 19 Frontliner - Weekend Warriors (Official Defqon 1 2013 Anthem)
+                00:00 20 Frequencerz - Victory Forever (Defqon.1 Anthem 2017)
+                00:00 21 Project One - Maximum Force (Defqon.1 Anthem 2018)
+            """.trimIndent()
+        ),
+        arrayOf(
+            TrackLists.SPD_12_YEARS,
+            """
+                00:00 01 W&W, Hardwell ft. Lil Jon - Live The Night
+                00:00 02  - ACDC - Thunderstruck (Shameless Bootleg)
+                00:00 03 MAKJ & Timmy Trumpet - Party Till We Die (feat. Andrew W.K.) [Extended Mix]
+                00:00 04  - Bon Jovi - Its My Life (TuneSquad Bootleg) full
+                00:00 05  - DOPEDROP - We Are The Stars
+                00:00 06 Dimitri Vegas & Like Mike vs. Ummet Ozcan - The Hum
+                00:00 07 W&W - Bigfoot
+                00:00 08 R3hab & KSHMR - Karate
+                00:00 09 KSHMR & Bassjackers - Memories (feat. Sirah)
+                00:00 10  - Freaks we will rock you (K3Leng Mashup)
+                00:00 11 Bassjackers, L3N - Bassjackers, D'Angello & Francis - All Aboard (Dimitri Vegas, Like Mike Edit)
+                00:00 12  - Cutting Crew - Died in Your Arms (TuneSquad Bootleg) Full
+                00:00 13 Kenneth G + AudioTwinz - Rave-Olution
+                00:00 14 Pitchback vs. Alesso vs. David Guetta vs. Linkin Park - Cosmic Dark vs. In The End (Hardwell UMF 2016 Mashup)
+                00:00 15 KEVU & Luke Alive - Final Countdown 2k16
+                00:00 16 Papa Roach vs Ummet Ozcan & Michael Jackson - Last Resort vs Smooth Criminal (Dimitri Vegas & Like Mike Mashup)
+                00:00 17 Eurythmics - Sweat Dreams (Ummet Ozcan Remix)
+                00:00 18 Snoop Dogg, Ummet Ozcan - The Next Episode (Ummet Ozcan Remix)
+                00:00 19 Hardscar - Pop Mix
+                00:00 20 The Prodigy - Omen
+                00:00 21 The Prodigy - Voodoo People (Pendulum Radio Edit)
+                00:00 22 Pendulum - Blood Sugar
+                00:00 23 Da Tweekaz ft. HALIENE - Bring Me To Life
+                00:00 24 Da Tweekaz - Komon
+                00:00 25 Ran-D - Zombie
+            """.trimIndent()
         )
     )
 
     @Test(dataProvider = "parseTrackListData")
     fun testParseTrackList(trackList: String, expectedTrackList: String) {
-        assertEquals(trackListEditor.parseTrackList(trackList), expectedTrackList)
+        assertEquals(trackListEditor.parseTrackListAsString(trackList), expectedTrackList)
+
+        val artists = trackListEditor.extractArtists(trackListEditor.parseTrackList(trackList))
+        println()
+        println(artists.joinToString("\n"))
+        println()
+        println(artists.joinToString())
     }
 }
