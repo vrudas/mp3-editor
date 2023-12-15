b02d2535abc4c4746b5a5e1d5c24c2cfcba84047
vrudas <vrudas4work@gmail.com>
2023-12-15 20:45:11 +0200

    Migrated MP3EditorTest to Kotlin

--- src/test/kotlin/io/mp3editor/MP3EditorTest.kt
+++ src/test/kotlin/io/mp3editor/MP3EditorTest.kt
@@ -1,56 +1,52 @@
-package io.mp3editor;
+package io.mp3editor
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 class MP3EditorTest {
     // TODO: 4/18/2016 IMPLEMENT TESTS
-
-    private MP3EditorV2 editor;
+    private lateinit var editor: MP3EditorV2
 
     @BeforeEach
-    public void init() {
-        editor = new MP3EditorV2();
+    fun init() {
+        editor = MP3EditorV2()
     }
 
     @Test
-    void testDeduplicateFileName() {
-        String newFileName = "newFileName";
-        String currentFileName = "fileName";
+    fun testDeduplicateFileName() {
+        val newFileName = "newFileName"
+        val currentFileName = "fileName"
 
-        String expectedFileName = String.format("%s (%s)", newFileName, currentFileName);
+        val expectedFileName = "$newFileName ($currentFileName)"
 
-        String deduplicatedFileName = editor.deduplicateFileName(newFileName, currentFileName);
+        val deduplicatedFileName = editor.deduplicateFileName(newFileName, currentFileName)
 
-        assertEquals(expectedFileName, deduplicatedFileName);
+        assertEquals(expectedFileName, deduplicatedFileName)
     }
 
     @Test
-    void testMakeFileName() {
-        String firstPart = "first";
-        String secondPart = "second";
-        String expectedFileName = String.format("%s - %s", firstPart, secondPart);
+    fun testMakeFileName() {
+        val firstPart = "first"
+        val secondPart = "second"
+        val expectedFileName = String.format("%s - %s", firstPart, secondPart)
 
-        String fileName = editor.makeFileName(firstPart, secondPart);
+        val fileName = editor.makeFileName(firstPart, secondPart)
 
-        assertEquals(expectedFileName, fileName);
+        assertEquals(expectedFileName, fileName)
     }
 
     @Test
-    void testPrepareTargetPath() {
-        String genreDir = "genre";
-        String fileName = "fileName";
-        String extension = ".extension";
+    fun testPrepareTargetPath() {
+        val genreDir = "genre"
+        val fileName = "fileName"
+        val extension = ".extension"
 
-        Path expectedPath = Paths.get(genreDir, fileName + extension);
+        val expectedPath = Paths.get(genreDir, fileName + extension)
 
-        Path path = editor.prepareTargetPath(genreDir, fileName, extension);
+        val path = editor.prepareTargetPath(genreDir, fileName, extension)
 
-        assertEquals(expectedPath, path);
+        assertEquals(expectedPath, path)
     }
 }
